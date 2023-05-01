6944cc0f67de01c73640c43f7e9f2379d13a4bfe
MihailoMotscny <117931269+MihailoMotscny@users.noreply.github.com>
2023-05-01 20:02:53 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,87 @@
-﻿using System;
+﻿bool Target = false;
 
-class Program
+Console.WriteLine("Enter a number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= 9; i += 1)
 {
-    static void Main(string[] args)
+    if (x % i == 0 && x != i)
     {
-        Console.WriteLine("Enter x");
-        double x = Convert.ToDouble(Console.ReadLine()); // замініть на бажане значення
-        double beta = 1; // початкове значення добутку елементів матриці
+        Target=true;
+        
+        break;
+        
 
-        // розрахунок Y
-        double Y = 0;
-        if (x <= 3)
-        {
-            Y = Math.Abs(Math.Pow(3, 2 * x + 1));
-        }
-        else if (3 < x && x < 20)
-        {
-            for (int i = 0; i < 21; i++)
-            {
-                beta *= (i + 21) * 3 < x ? 2 : 1;
-            }
-            Y = Math.Abs(4 - beta);
-        }
-        else
-        {
-            for (int i = 6; i <= 3; i++)
-            {
-                beta *= 2;
-            }
-            Y = Math.Abs(beta);
-        }
+    }
+    
 
-        Console.WriteLine("Y = " + Y);
+}
+
+if (Target == true)
+{
+
+    for(int h=1;Target!=false;h+=1)
+    {
+        string num = h.ToString();
+        int siz = num.Length;
+        int r = 0;
+        int predfin = 1;
+
+        while(r != siz)
+        {
+            foreach (char c in num)
+            {
+                int numint= (int)Char.GetNumericValue(c);
+                predfin *= numint;
+                r += 1;
+                if (predfin == x)
+                {
+                    Console.WriteLine("Ansver="+h);
+                    Target = false;
+                    break;
+                }
+            }
+
+        }
+               
     }
 }
+else
+{
+    Console.WriteLine("Error404");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
